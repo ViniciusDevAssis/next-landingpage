d021aeccc54f066fe4331b3d96d5f51a30689b46
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-02-20 18:15:17 -0300

    creating the hero section (not finished yet)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,7 +49,8 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
-  			}
+  			},
+				lp_red: '#E84C3D'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
